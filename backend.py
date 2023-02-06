444449674851b2ddf262969cac4083a55f178192
WenyiLuo03 <124179546+WenyiLuo03@users.noreply.github.com>
2023-02-05 20:55:36 -0500

    Initial Commit

---
+++ backend.py
@@ -0,0 +1,15 @@
+import requests
+
+API_KEY="8a1c25a99b7e5f85446ca6177c1bf3b9"
+
+def get_data(place, forecast_days):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+if __name__ == "__main__":
+    get_date(place)
